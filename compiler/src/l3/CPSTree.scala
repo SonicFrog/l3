0714c8129c2f28c0692df9eb7bc4d19464ba37c7
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2016-03-07 13:19:11 +0100

    Assignment 3: Value Representation

--- compiler/src/l3/CPSTree.scala
+++ compiler/src/l3/CPSTree.scala
@@ -12,7 +12,42 @@ trait CPSTreeModule {
   type TestPrimitive
   type Literal
 
-  sealed trait Tree
+  sealed trait Tree {
+    /**
+     * Produce a new version of the tree where all names have been
+     * substituted according to the given (partial) substitution.
+     */
+    def subst(s: Substitution[Name]): Tree = {
+      def substIn(t: Tree): Tree = t match {
+        case LetL(name, value, body) =>
+          LetL(s(name), value, substIn(body))
+        case LetP(name, prim, args, body) =>
+          LetP(s(name), prim, args map s, substIn(body))
+        case LetC(cnts, body) =>
+          val substCnts = cnts map {
+            case CntDef(name, args, body) =>
+              CntDef(s(name), args map s, substIn(body))
+          }
+          LetC(substCnts, substIn(body))
+        case LetF(funs, body) =>
+          val substFuns = funs map {
+            case FunDef(name, retC, args, body) =>
+              FunDef(s(name), s(retC), args map s, substIn(body))
+          }
+          LetF(substFuns, substIn(body))
+        case AppC(cont, args) =>
+          AppC(s(cont), args map s)
+        case AppF(fun, retC, args) =>
+          AppF(s(fun), s(retC), args map s)
+        case If(cond, args, thenC, elseC) =>
+          If(cond, args map s, s(thenC), s(elseC))
+        case Halt(arg) =>
+          Halt(s(arg))
+      }
+
+      substIn(this)
+    }
+  }
 
   case class LetL(name: Name, value: Literal, body: Tree) extends Tree
   case class LetP(name: Name, prim: ValuePrimitive, args: Seq[Name], body:Tree)
@@ -38,5 +73,17 @@ object SymbolicCPSTreeModule extends CPSTreeModule {
   type ValuePrimitive = L3ValuePrimitive
   type TestPrimitive = L3TestPrimitive
   type Literal = CL3Literal
+}
+
+/**
+ * Module for "low-level" CPS trees: the only literal values are
+ * integers, and the primitives work on integers and/or pointers to
+ * heap-allocated blocks.
+ */
+object SymbolicCPSTreeModuleLow extends CPSTreeModule {
+  type Name = Symbol
+  type ValuePrimitive = CPSValuePrimitive
+  type TestPrimitive = CPSTestPrimitive
+  type Literal = Int
 }
 
