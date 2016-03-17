4afbf3c06c9674be42b84a2f29db27cce9ce916c
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2016-03-17 15:35:21 +0100

    Assignment 4
    Note: something is wrong with CPSInterpreterLow.
    Previously it used to need modification here, as it ran without CPS
    translation, so it wrapped all functions in blocks.
     but this patches are already applied.
    
    Does it even work now?

--- compiler/src/l3/Main.scala
+++ compiler/src/l3/Main.scala
@@ -22,6 +22,7 @@ object Main extends MainHelper {
               andThen CPSValueRepresenter
               andThen treePrinter("Tree in CPS-low")
               andThen passThrough(SymbolicCPSTreeLowChecker)
+              andThen CPSHoister
               andThen CPSInterpreterLow
           )
           try {
