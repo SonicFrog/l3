1808474d99462492d2ef023ae0ce4ea97b50216d
Ogier Bouvier <ogier.bouvier@epfl.ch>
2016-04-13 10:33:43 +0200

    Free vars computation

--- compiler/src/l3/CPSValueRepresenter.scala
+++ compiler/src/l3/CPSValueRepresenter.scala
@@ -5,13 +5,13 @@ import l3.{ SymbolicCPSTreeModule => H }
 import l3.{ SymbolicCPSTreeModuleLow => L }
 
 /**
- * Value-representation phase for the CPS language. Translates a tree
- * with high-level values (blocks, integers, booleans, unit) and
- * corresponding primitives to one with low-level values (blocks
- * and integers only) and corresponding primitives.
- *
- * @author Michel Schinz <Michel.Schinz@epfl.ch>
- */
+  * Value-representation phase for the CPS language. Translates a tree
+  * with high-level values (blocks, integers, booleans, unit) and
+  * corresponding primitives to one with low-level values (blocks
+  * and integers only) and corresponding primitives.
+  *
+  * @author Michel Schinz <Michel.Schinz@epfl.ch>
+  */
 
 object CPSValueRepresenter extends (H.Tree => L.Tree) {
   def apply(tree: H.Tree): L.Tree =
@@ -58,13 +58,13 @@ object CPSValueRepresenter extends (H.Tree => L.Tree) {
         tempLetP(CPSSub, Seq(n, c1)) { nRdy =>
           tempLetP(CPSArithShiftR, Seq(s, c1)) { sRdy =>
             tempLetP(CPSArithShiftL, Seq(nRdy, sRdy)) { r =>
-                L.LetP(name, CPSOr, Seq(r, c1), transform(body)) } } } }
+              L.LetP(name, CPSOr, Seq(r, c1), transform(body)) } } } }
 
     case H.LetP(name, L3IntArithShiftRight, Seq(n, s), body) =>
       tempLetL(1) { c1 =>
-          tempLetP(CPSArithShiftR, Seq(s, c1)) { sRdy =>
-            tempLetP(CPSArithShiftR, Seq(n, sRdy)) { r =>
-                L.LetP(name, CPSOr, Seq(r, c1), transform(body)) } } }
+        tempLetP(CPSArithShiftR, Seq(s, c1)) { sRdy =>
+          tempLetP(CPSArithShiftR, Seq(n, sRdy)) { r =>
+            L.LetP(name, CPSOr, Seq(r, c1), transform(body)) } } }
 
     case H.LetP(name, L3IntBitwiseAnd, args, body) =>
       L.LetP(name, CPSAnd, args, transform(body))
@@ -184,36 +184,59 @@ object CPSValueRepresenter extends (H.Tree => L.Tree) {
     case _ => ??? // TODO Handle other cases
   }
 
+  private def freeVars(tree : H.Tree) : Seq[H.Name] = {
+    def F(tree : H.Tree) : Set[H.Name] = tree match {
+      case H.LetL(name, _, e) => F(e) - name
+      case H.LetP(name, _, args, e) => (F(e) - name) union args.toSet
+      case H.LetC(cnts, e) => F(e) union (cnts.map(contFV).fold(Set())(_ union _))
+      case H.LetF(funs, e) => F(e) union (funs.map(funFV).fold(Set())(_ union _))
+      case H.AppC(_, args) => args.toSet
+      case H.AppF(fun, _, args) => args.toSet
+      case H.If(_, args, _, _) => args.toSet
+      case H.Halt(arg) => Set(arg)
+    }
+
+    def funFV(f : H.FunDef) : Set[H.Name] = f match {
+      case H.FunDef(_, _, args, e) => F(e) &~ args.toSet
+    }
+
+    def contFV(c : H.CntDef) : Set[H.Name] = c match {
+      case H.CntDef(_, args, e) => F(e) &~ args.toSet
+    }
+
+    F(tree).toSeq
+  }
+
   // Tree builders
 
   /**
-   * Call body with a fresh name, and wraps its resulting tree in one
-   * that binds the fresh name to the given literal value.
-   */
+    * Call body with a fresh name, and wraps its resulting tree in one
+    * that binds the fresh name to the given literal value.
+    */
   private def tempLetL(v: Int)(body: L.Name => L.Tree): L.Tree = {
     val tempSym = Symbol.fresh("t")
     L.LetL(tempSym, v, body(tempSym))
   }
 
   /**
-   * Call body with a fresh name, and wraps its resulting tree in one
-   * that binds the fresh name to the result of applying the given
-   * primitive to the given arguments.
-   */
+    * Call body with a fresh name, and wraps its resulting tree in one
+    * that binds the fresh name to the result of applying the given
+    * primitive to the given arguments.
+    */
   private def tempLetP(p: L.ValuePrimitive, args: Seq[L.Name])
-                      (body: L.Name => L.Tree): L.Tree = {
+    (body: L.Name => L.Tree): L.Tree = {
     val tempSym = Symbol.fresh("t")
     L.LetP(tempSym, p, args, body(tempSym))
   }
 
   /**
-   * Generate an If tree to check whether the least-significant bits
-   * of the value bound to the given name are equal to those passed as
-   * argument. The generated If tree will apply continuation tC if it
-   * is the case, and eC otherwise. The bits should be ordered with
-   * the most-significant one first (e.g. the list (1,1,0) represents
-   * the decimal value 6).
-   */
+    * Generate an If tree to check whether the least-significant bits
+    * of the value bound to the given name are equal to those passed as
+    * argument. The generated If tree will apply continuation tC if it
+    * is the case, and eC otherwise. The bits should be ordered with
+    * the most-significant one first (e.g. the list (1,1,0) represents
+    * the decimal value 6).
+    */
   private def ifEqLSB(arg: L.Name, bits: Seq[Int], tC: L.Name, eC: L.Name)
       : L.Tree =
     tempLetL(bitsToIntMSBF(bits map { b => 1 } : _*)) { mask =>
