2b6f7fbe7c7965de3f4ada985323be2523d84e85
Ogier Bouvier <ogier.bouvier@epfl.ch>
2016-04-14 10:43:34 +0200

    Some closures tests

--- compiler/test/l3/test/CPSValueRepresentation_Blackbox.scala
+++ compiler/test/l3/test/CPSValueRepresentation_Blackbox.scala
@@ -3,9 +3,35 @@ package l3.test
 import l3.test.infrastructure.CPSLowTest
 import l3.test.ok.AllOKTests
 
+import org.junit.Test
+
 /** Blackbox testing for entire program outputs */
 class CPSValueRepresentation_Blackbox extends CPSLowTest with AllOKTests {
 
   val compileAndInterpret = (src: String) => testCPSLowProgramOutput(source = src)
   // TODO: Add other specific tests here
+
+  @Test def testMakePrinter = compileAndInterpret("""
+     (def make-printer (fun (x)
+                         (fun ()
+                           (@byte-write (@char->int x))
+                           (@byte-write (@char->int 'K')))))
+     ((make-printer 'O'))
+      """)
+
+  @Test def testMakePrinterComplex = compileAndInterpret("""
+    (def make-printer (fun (x y)
+                         (fun ()
+                           (@byte-write (@char->int x))
+                           (@byte-write (@char->int y)))))
+    ((make-printer 'O' 'K'))
+    """)
+
+  @Test def testLetFun = compileAndInterpret("""
+    (let* ((x 'O') (y 'K'))
+          (letrec ((f (fun ()
+                       (@byte-write (@char->int x))
+                       (@byte-write (@char->int y)))))
+                   (f)))
+  """)
 }
