6c5e927be85c0b16cc0cbc0ab51ea906c29131b3
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2016-05-18 16:49:43 +0200

    Add vm and precompiled examples

---
+++ vm/src/memory.h
@@ -0,0 +1,47 @@
+#ifndef __MEMORY_H
+#define __MEMORY_H
+
+#include <stdlib.h>
+#include "vmtypes.h"
+
+#define GC_NOFREE       0 // the never-free garbage collector
+#define GC_MARK_N_SWEEP 1 // the mark and sweep garbage collector
+
+// the type of garbage collector to use
+// TODO Switch to your mark-n-sweep gc once you're done
+#define GC_VERSION GC_NOFREE
+
+typedef enum {
+  tag_String = 200,
+  tag_RegisterFrame = 201,
+  tag_Function = 202
+} tag_t;
+
+/* Returns a string identifying the memory system */
+char* memory_get_identity();
+
+/* Setup the memory allocator and garbage collector */
+void memory_setup(size_t total_size);
+
+/* Tear down the memory */
+void memory_cleanup();
+
+/* Get first memory address */
+void* memory_get_start();
+
+/* Get last memory address */
+void* memory_get_end();
+
+/* Set the heap start, following the code area */
+void memory_set_heap_start(void* heap_start);
+
+/* Allocate block, return physical pointer to the new block */
+value_t* memory_allocate(tag_t tag, unsigned int size);
+
+/* Unpack block size from a physical pointer */
+size_t memory_get_block_size(value_t* block);
+
+/* Unpack block tag from a physical pointer */
+tag_t memory_get_block_tag(value_t* block);
+
+#endif
